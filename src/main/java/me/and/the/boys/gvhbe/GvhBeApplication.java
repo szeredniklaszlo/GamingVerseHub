3a92327b0bcc58a7d94681acc35bbd0c18dc7ee2
szeredniklaszlo <40238284+szeredniklaszlo@users.noreply.github.com>
2025-05-08 21:12:48 +0200

    Init project.

---
+++ src/main/java/me/and/the/boys/gvhbe/GvhBeApplication.java
@@ -0,0 +1,13 @@
+package me.and.the.boys.gvhbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GvhBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GvhBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/and/the/boys/gvhbe/GvhBeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.and.the.boys.gvhbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class GvhBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
